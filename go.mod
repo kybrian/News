323337f706a64b05ea93640fae3b94c894826ba1
kybrian <yegonk.se@gmail.com>
2022-10-18 04:30:32 +0300

    Create a web server, read variables from the environment, template, navigation bar and serving static files

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example.com/m
 
 go 1.19
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- main.go
+++ main.go
@@ -1,22 +1,44 @@
 package main
 
 import (
-	"net/http"
-	"os"
+	"html/template"
+	"log"
+	"net/http" // HTTP Client and server implementations
+	"os"       // Access OS functionality
+
+	"github.com/joho/godotenv"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Let's Go</h1>"))
+var tpl = template.Must(template.ParseFiles("index.html")) // template.ParseFiles parses index.html and validates it
+// It's wrapped with template.Must so that the code panics if an error occurs when parsing
+
+func indexHandler(w http.ResponseWriter, r *http.Request /* Register http requests handler*/) {
+	// w.Write([]byte("<h1>Let's Go</h1>"))
+	tpl.Execute(w, nil)
 }
 
 func main() {
+	// Read variables from environment
+	err := godotenv.Load() // read the .env file and loads the set environment variables into the environment making them accessible through os.Getenv() method
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	// Writing our web server
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
 
-	mux := http.NewServeMux()
+	// Instantiate a file server object
+	fs := http.FileServer(http.Dir("assets"))
+
+	mux := http.NewServeMux() // Create a http request multiplexer assigned to the mux variable
+
+	// Essentially, a request multiplexer matches the URL of incoming requests against a list of registered patterns, and calls the associated handler for the pattern whenever a match is found
 
 	mux.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":"+port, mux)
+	// tell router to use file server object for all paths beginning with the /assets/ prefix
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs)) // The http.StripPrefix() method modifies the request URL by stripping off the specified prefix before forwarding the handling of the request to the http.Handler in the second parameter.
+	http.ListenAndServe(":"+port, mux)                       // Start server
 }
